1c6722f2abc5cee97eb276237430bff154920738
Ilya <ilyamishencko@gmail.com>
2024-09-21 10:00:50 +0300

    one-test

---
+++ test_UI.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from  time import sleep
+
+
+browser = webdriver.Chrome()
+browser.get('https://magento.softwaretestingboard.com/')
+sleep(1)
+sale_link = browser.find_element(By.ID, 'ui-id-8')
+sale_link.click()
+title = browser.find_element(By.TAG_NAME,'h1')
+assert title.text == 'Sale'
