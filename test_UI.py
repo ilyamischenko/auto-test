65e7014a35e8a4522508107e0b51710970dc5090
Ilya <ilyamishencko@gmail.com>
2024-09-23 19:06:57 +0300

    gitIgnore

--- test_UI.py
+++ test_UI.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from  time import sleep
 
-
-browser = webdriver.Chrome()
-browser.get('https://magento.softwaretestingboard.com/')
-sleep(1)
-sale_link = browser.find_element(By.ID, 'ui-id-8')
-sale_link.click()
-title = browser.find_element(By.TAG_NAME,'h1')
-assert title.text == 'Sale'
+def test_sale():
+    browser = webdriver.Chrome()
+    browser.get('https://magento.softwaretestingboard.com/')
+    sleep(1)
+    sale_link = browser.find_element(By.ID, 'ui-id-8')
+    sale_link.click()
+    title = browser.find_element(By.TAG_NAME,'h1')
+    assert title.text == 'Sale'
+#запуск через pytest
